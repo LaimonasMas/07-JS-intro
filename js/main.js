0c499179e2716c5437325e29b30b48b3d55fa85a
LaimonasMas <71811684+LaimonasMas@users.noreply.github.com>
2020-11-11 15:48:18 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,135 @@
+// spausdiname sveika skaiciu 
+
+console.log(84531);
+
+// du skaiciai vs desimtainis - kaip veikia ir koks yra kablelis
+
+console.log(3, 14);
+console.log(3.14);
+
+// kaip spausdinamas tekstas
+
+console.log("Labas rytas");
+console.log("Laimonas Masionis");
+
+console.log("Gera paskaita");
+console.log("maziau teksto");
+
+console.log("jau gerai gaunasi");
+
+
+/*********************
+ * KINTAMIEJI
+ **************************/
+/* Iniciavimo budai:
+
+    const - default, tai musu pirmasis pasirinkimas
+    let - galima, tik jei yra reikalas keisti reiksme
+    var - nenaudotinas
+*/
+
+
+    /*
+    Kintamuju tipai (pagal reiksmes):
+        skaiciai (number) - 2020, 3.14, -7, -5.144
+        tekstai (string) - "labas", 'ka tu?',  ":)"
+        logine (boolean) - true, false
+        sarasai (array) - []
+        objektai (object) - {} 
+    */
+
+
+
+
+
+ const pirmasSkaicius = 2;
+ const antrasSkaicius = 3;
+
+ console.log("Pirmasis skaicius turetu buti 2.");
+ console.log(pirmasSkaicius);
+
+ console.log("Antras skaicius turetu buti 3.");
+ console.log(antrasSkaicius);
+
+ const suma = pirmasSkaicius + antrasSkaicius;
+ console.log(suma);
+
+ console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+ 
+ const vardas = 'Laimonas';
+ const amzius = 99
+ console.log(vardas);
+ console.log(amzius);
+
+ // Laimonas yra 99 metu amziaus.
+ // ________ yra __ metu amziaus.
+
+ const sakinys = vardas + ' yra ' + amzius + ' metu amziaus.';
+ console.log(sakinys);
+
+ const suo = 'Rikis'
+ const maistas = 'kaulas'
+
+ // Rikis megstamas maistas yra kaulas.
+
+ const sakinys2 = suo + ' megstamas maistas yra ' + maistas + '.';
+ console.log(sakinys2);
+
+
+ const heroName = 'Chuck';
+ const heroLastname = 'Norris';
+ const birth = 1940;
+ const date = 2020;
+ const amzius1 = date - birth;
+ console.log(amzius1);
+
+ 
+
+ // Chuck Norris (80)
+ // Jo vardas: Chuck.
+
+
+ const sakinys3 = heroName + ' ' + heroLastname + ' ' + '(' + amzius1 + ')';
+ console.log(sakinys3);
+
+ console.clear();
+
+ let pinigine = 7;
+ const sausis = 100;
+ const vasaris = 200;
+ const kovas = 70;
+ const balandis = -200;
+
+console.clear;
+
+ // 107
+ // 307
+ // 377
+
+pinigine = pinigine + sausis;
+console.log(pinigine);
+
+pinigine = pinigine + vasaris;
+console.log(pinigine);
+
+pinigine = pinigine + kovas;
+console.log(pinigine);
+
+pinigine = pinigine + balandis;
+console.log(pinigine);
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
